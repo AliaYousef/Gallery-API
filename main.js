d37b1ea5b5e81fba5303b00e4e2a0d0df7e3066f
alia yousef <aliayousef33@gmail.com>
2019-10-24 12:28:08 -0700

    first push

---
+++ main.js
@@ -0,0 +1,38 @@
+document.getElementById('aa').addEventListener('click', addData);
+let arr=[];
+const myheaders = new Headers();
+myheaders.append('Authorization', 'Bearer ETnS0SprqU6-h4vaPmVUxO0SJ4bOZGyqO7QU');
+fetch('https://gorest.co.in/public-api/photos' , {
+        method: 'GET',
+        headers: myheaders,
+    })
+    .then(Response =>
+        Response.json())
+    .then(result =>{
+        arr= result.result,
+        addData()
+    });
+
+function addData(){
+    const html = arr.map((item, index) => {
+        return  '<img class="img1" src="' +item.thumbnail + '" data-id="' + index + '" onClick="togglePhoto(event)">';
+    });
+    document.getElementById('add1').innerHTML = html.join('');
+}
+
+function togglePhoto(event){
+    console.log(arr);
+    document.getElementById("aa").style.visibility = "visible"
+    const index = event.target.getAttribute('data-id');
+    console.log(index)
+    document.getElementById("add").innerHTML = '<div class= "imgDiv"><img   src="' + arr[index].url + '" class="img3"><div class="text-block">'+arr[index].title+' </div></div>';
+}
+document.getElementById("close").addEventListener('click', close);
+function close() {
+    document.getElementById("aa").style.visibility = "hidden";
+}
+
+// function addData (){
+//   console.log(arr);
+//   renderGallery(arr) 
+// }
